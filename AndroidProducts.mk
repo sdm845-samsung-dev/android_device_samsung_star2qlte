sm8250-samsung/android_device_samsung_y2q

Initial skeleton device tree

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_y2q.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_y2q-user \
+    lineage_y2q-userdebug \
+    lineage_y2q-eng

BoardConfig.mk
@@ -1,0 +1,10 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Include the common OEM chipset BoardConfig.
+include device/samsung/sm8250-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/y2q

device.mk
@@ -1,0 +1,8 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the common OEM chipset makefile.
+$(call inherit-product, device/samsung/sm8250-common/common.mk)

sdm845-samsung-dev/android_device_samsung_star2qlte

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_star2qlte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_star2qlte-user \
+    lineage_star2qlte-userdebug \
+    lineage_star2qlte-eng

BoardConfig.mk
@@ -1,0 +1,10 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Include the common OEM chipset BoardConfig.
+include device/samsung/sdm845-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/star2qlte

device.mk
@@ -1,0 +1,8 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the common OEM chipset makefile.
+$(call inherit-product, device/samsung/sdm845-common/common.mk)